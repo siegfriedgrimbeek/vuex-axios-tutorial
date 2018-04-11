7c50e57a9c61548b9e3f7f8e46e7aac018dd91ae
Siegfried <siegfried.grimbeek@gmail.com>
2018-04-11 23:10:44 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app">
+    <div id="header-nav" class="nav-bars">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/words">Words</router-link> |
+        <router-link :to="latestWord">{{ latestWord }}</router-link>
+    </div>
+    <router-view/>
+    <div id="footer-nav" class="nav-bars">
+      My Dictionary 2018 - {{ wordCount }} words has been added.
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      wordCount: 0,
+      latestWord: 'Latest_Word'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+
+  .nav-bars{
+    background: cornflowerblue;
+    position: fixed;
+    width: 100%;
+    padding: 10px;
+    color: white;
+
+    a {
+      color: white;
+      text-decoration: none;
+    }
+  }
+
+  #header-nav {
+    top: 0;
+    left: 0;
+  }
+
+  #footer-nav {
+    bottom: 0;
+    left: 0;
+  }
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="home">
+    <div class="input-container">
+      <div class="input-group">
+        <input type="text" v-model="word" />
+        <button @click="submitWord" type="submit">Add Word</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'My Personal Dictionary',
+      word: ''
+    }
+  },
+  methods: {
+    submitWord (e) {
+      alert(this.word)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+.home{
+  display: flex;
+  align-items:center;
+  justify-content: center;
+  height: calc(100vh);
+
+  .input-container{
+      max-width: 500px;
+      width:100%;
+
+      .input-group{
+        display:flex;
+        justify-content: space-between;
+
+        input {
+          box-sizing: border-box;
+          width: calc(100% - 150px);
+          padding: .5rem;
+          border: 2px solid cornflowerblue;
+          font-size: 1rem;
+          border-radius: 0;
+          -webkit-appearance: none;
+        }
+
+        button{
+          width:150px;
+          height:50px;
+          -webkit-appearance: none;
+          background: cornflowerblue;
+          color: white;
+          text-transform: uppercase;
+          border: none;
+          font-weight:bold;
+          font-size: 12pt;
+        }
+      }
+  }
+}
+</style>
